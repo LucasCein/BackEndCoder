eb746c2e82a1ef411ff2c2b7e1d85ec9484cc8f3
Lucas Cein <lucascein82@gmail.com>
2023-11-14 12:46:48 -0300

    Desafio2

---
+++ Desafio2/ProductManager.js
@@ -0,0 +1,106 @@
+import fs from 'fs'
+export default class ProductManager {
+    constructor(url) {
+        this.products = [];
+        this.path = url
+    }
+
+    async getProducts() {
+        if (fs.existsSync(this.path)) {
+            try {
+
+                const data = await fs.promises.readFile(this.path, 'utf-8')
+                this.products = JSON.parse(data)
+            } catch (err) {
+                console.log(err)
+            }
+
+        }
+        return this.products
+
+    }
+
+    async addProduct(producto) {
+        await this.getProducts()
+        if (this.products.length > 0) {
+            producto.id = this.products[this.products.length - 1].id + 1
+        }
+        else {
+            producto.id = 1
+        }
+        let { title, description, price, thumbnail, code, stock } = producto
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.log("Todos los campos son obligatorios");
+            return;
+        }
+        if (this.products.some((product) => product.id === producto.id)) {
+            console.log("El código ya existe");
+            return;
+        }
+        const newProduct = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock,
+            id: producto.id++
+        };
+        this.products.push(newProduct);
+        try {
+            await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, '\t'))
+            console.log('Se ha agregado el producto')
+        } catch(err) {
+            console.log(err)
+        }
+
+    }
+
+    async getProductById(id) {
+        try{
+            await this.getProducts()
+            if(this.products.length === 0){
+                console.log("Not found")
+                return
+            }
+            else{
+                const product = this.products.find(product => product.id === id)
+                return product
+            }
+        }catch(err){
+            console.log(err)
+        }
+    }
+    async updateProduct(id, update){
+        await this.getProducts()
+        const index = this.products.findIndex(product => product.id === id)
+        if(index === -1){
+            console.log("Not found")
+            return
+        }
+        this.products[index] = {...this.products[index], ...update}
+        try{
+            await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, '\t'))
+            console.log('Se ha actualizado el producto')
+        }catch(err){
+            console.log(err)
+        }
+    }
+    async deleteProduct(id){
+        await this.getProducts()
+        const index = this.products.findIndex(product => product.id === id)
+        if(index === -1){
+            console.log("Not found")
+            return
+        }
+        this.products.splice(index, 1)
+        try{
+            await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, '\t'))
+            console.log('Se ha eliminado el producto')
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+}
+
---
+++ Desafio2/index.js
@@ -0,0 +1,33 @@
+import ProductManager from "./ProductManager.js";
+
+const product=new ProductManager('./products.json')
+const env = async () => {
+    await product.addProduct({
+        title: 'Escuadra',
+        description: 'Escuadra de 2.5mm',
+        price: 123.45,
+        thumbnail: 'Sin imagen',
+        code: 'abc123',
+        stock: 25
+    })
+    await product.addProduct({
+        title: 'Calculadora',
+        description: 'Calculadora de 2.5mm',
+        price: 123.45,
+        thumbnail: 'Sin imagen',
+        code: 'abc123',
+        stock: 25
+    })
+    console.log(await product.getProducts())
+    console.log('producto por id:',await product.getProductById(1))
+    await product.updateProduct(1,{
+        title: 'Calculadora',
+        description: 'Calculadora de 2.5mm',
+        price: 123.45,
+        thumbnail: 'Sin imagen',
+        code: 'abc123',
+        stock: 25
+    })
+    await product.deleteProduct(1)
+}
+env()
