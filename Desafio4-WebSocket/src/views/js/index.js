ba33cead4bb0ae5cc34ad6c6a12e1ce1a82d051d
Lucas Cein <lucascein82@gmail.com>
2023-12-05 18:56:46 -0300

    Desafio 4 fix

--- Desafio4-WebSocket/src/views/js/index.js
+++
@@ -1,7 +0,0 @@
-
-const socket = io();
-
-socket.on('actualizarProductos', (productos) => {
-    const lista = document.getElementById('lista-productos');
-    lista.innerHTML = productos.map(p => `<li>${p.nombre} - $${p.precio}</li>`).join('')
-})
